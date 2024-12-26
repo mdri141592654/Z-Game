88786aeccc01b00c13e822cfb47f7ab7ea920da2
mdri141592654 <wieser_mathias@yahoo.de>
2024-12-26 11:58:30 +0100

    Update game.js

--- game.js
+++ game.js
@@ -7,11 +7,23 @@ document.getElementById('language').addEventListener('change', (e) => {
     document.getElementById('use-materials').textContent = 'Materialien verwenden';
     document.getElementById('sleep').textContent = 'Schlafen';
     document.getElementById('day-counter').innerHTML = '08:00 am<br>Tag: 00';
+    document.getElementById('resources').innerHTML = `
+      Nahrung: 00<br>
+      Medizin: 00<br>
+      Munition: 00<br>
+      Baumaterial: 00
+    `;
   } else if (lang === 'en') {
     document.getElementById('scavenge').textContent = 'Scavenge';
     document.getElementById('targeted-scavenge').textContent = 'Targeted Scavenge';
     document.getElementById('use-materials').textContent = 'Use Materials';
     document.getElementById('sleep').textContent = 'Sleep';
     document.getElementById('day-counter').innerHTML = '08:00 am<br>Day: 00';
+    document.getElementById('resources').innerHTML = `
+      Food: 00<br>
+      Medicine: 00<br>
+      Ammo: 00<br>
+      Materials: 00
+    `;
   }
 });
