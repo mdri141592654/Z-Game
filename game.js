88c33868e6a47ee88b8cf17edb4b857ef63b47ed
mdri141592654 <wieser_mathias@yahoo.de>
2025-01-19 15:41:13 +0100

    Update game.js

--- game.js
+++ game.js
@@ -9,6 +9,22 @@ function openMenu(menuId) {
 
     const background = document.getElementById('background');
 
+    // inventar bei start
+function setInitialInventory() {
+    const initialMunition = 15; // Start-Menge Munition
+    const initialFood = 10; 
+    const initialMedicine = 5;
+    const initialMateial =10;
+
+    // Setze die Werte in den HTML-Elementen
+    document.getElementById('munition-count').textContent = initialMunition;
+    document.getElementById('gegenstand1-count').textContent = initialGegenstand1;
+    document.getElementById('gegenstand2-count').textContent = initialGegenstand2;
+}
+
+// Aufruf der Funktion beim Spielstart
+setInitialInventory();
+
     // Überblenden nur bei Zurückgehen aus Menü 141
     if (menuId === 'menu-141') {
         const loadingBar = document.getElementById('sleeping-bar');
